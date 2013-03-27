4af21fbbff65cead0982a94832c3135a7d2c133d
amonfog <amonfog@gmail.com>
2013-03-27 14:44:43 +0100

    Update 1: Add script to empty div with class 'replacerdiv' and repopulate with pedifined html text | update 2: add script to call json file jsonbasic script now version 1.0

--- app/assets/javascripts/articles-home.js
+++ app/assets/javascripts/articles-home.js
@@ -1,1 +1,6 @@
 // This is the javascript file for articles home. all code for articles should go here
+function drama() {
+	// we are looking for a div with the classname ( . like in css) replacercontent. this div will be repopulated with the content after emptying
+	$('.replacercontent').empty();
+	$('.replacercontent').append('hier kom een hook die de json uitleest en er random conent in zet.');
+}
--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,2 +1,1 @@
 // this is de default javascript file for the application. functions that should be implemented on all article pages go here
-alert("Goedzo, je hebt je eigen probleem opgelost!");
