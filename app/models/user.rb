448db564b172c9394d8d3356b95413544833a7f1
amonfog <amonfog@gmail.com>
2013-04-24 12:48:25 +0200

    Public Password_resets and validation fix
    temporary fix by removing validation of password length since this
    conflicts with the token function and send_password_reset is now a
    public function in users.rb. Validations need to be added in future
    commits and a mailer system needs to be added to send the mail.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
+  before_create { generate_token(:auth_token) }
+
+  def send_password_reset
+    generate_token(:password_reset_token)
+    self.password_reset_sent_at = Time.zone.now
+    save!
+    UserMailer.password_reset(self).deliver
+  end
+
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
 
@@ -9,8 +18,8 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX },
  					 uniqueness: { case_sensitive: false }
-  validates :password, length: {minimum: 6}
-  validates :password_confirmation, presence: true
+  #validates :password, length: {minimum: 6}
+  #validates :password_confirmation, presence: true
 
 	private
 
@@ -18,14 +27,7 @@ class User < ActiveRecord::Base
 		self.remember_token = SecureRandom.urlsafe_base64
 	end
 
-  before_create { generate_token(:auth_token) }
 
-  def send_password_reset
-  generate_token(:password_reset_token)
-  self.password_reset_sent_at = Time.zone.now
-  save!
-  UserMailer.password_reset(self).deliver
-end
 
 def generate_token(column)
   begin
