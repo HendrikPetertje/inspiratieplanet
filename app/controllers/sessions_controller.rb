16b12cdf06d79e2dc83cbd9ca9d804aa89b96c1f
amonfog <amonfog@gmail.com>
2013-04-25 14:43:41 +0200

    Spellerror in sessions_controller.rb
    fixed a spelling error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   	user = User.find_by_email(params[:session][:email].downcase)
 	if user && user.authenticate(params[:session][:password])
 		sign_in user
-		redirect_to_or user
+		redirect_to user
 	else
 		flash.now[:error] = 'Invalid e-mail / password combination'
   	render 'new'
