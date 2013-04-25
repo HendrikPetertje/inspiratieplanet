8af63213b22464ee894664f3d911c781b94170e8
amonfog <amonfog@gmail.com>
2013-04-25 12:29:12 +0200

    Adds relations between users and posts
    The user_model and posts_model have added relations where a user can
    have many posts and a post belongs to a user

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ActiveRecord::Base
   attr_accessible :content, :picture, :tags, :title, :user_id
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_create { generate_token(:auth_token) }
+has_many :articles
 
   def send_password_reset
     generate_token(:password_reset_token)
