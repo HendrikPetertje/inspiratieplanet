65b0289478acdc2238fa96c5c614de712f125bc5
amonfog <amonfog@gmail.com>
2013-05-08 11:17:50 +0200

    Fix picture uploader for windows machines
    This commit changes the picture format for imagemagic to match
    functionality on windows machines

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -51,7 +51,7 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
    version :thumb do
-     process :resize_to_limit => [250, nil]
+     process :resize_to_fill => [250]
    end
 
   # Add a white list of extensions which are allowed to be uploaded.
