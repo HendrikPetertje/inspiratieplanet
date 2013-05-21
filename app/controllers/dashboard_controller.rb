d661c5533740acf7d07a100a640bb042affb88c8
Tjerk Jansma <tjerkjansma@msn.com>
2013-05-21 15:27:09 +0200

    in progress: load more content into user dashboard
    The articles in the user dashboard should be able to expand through a
    button or endless scrolling.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,24 @@
+require 'will_paginate/array'
+
 class DashboardController < ApplicationController
 before_filter :authenticate_user!
 
   def user
-  	@articles = current_user.articles.order("articles.created_at desc").limit(10)
+  	@articles = current_user.articles.order("articles.created_at desc").limit(4)
   end
+
+  def paginate
+  	@articles = Article.paginate(:page => params[:page], :per_page => 4)
+  end
+ 
+
+  #  def user # uploadedArticles
+  #  	@user = User.find(params[:id])
+  #  	@userarticles = @user.articles(:limit => 4)
+  #    respond_to do |format|
+  #     format.html 
+  #     format.json { render json: @userarticles }
+  #   end
+  # end
 
 end
