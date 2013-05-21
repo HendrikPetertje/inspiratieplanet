85d56ac9b84d9bf953fbc03f027baf1aa8c54c0b
amonfog <amonfog@gmail.com>
2013-05-21 12:01:54 +0200

    Add microthumb for comments
    Adds microthumb options and hooks for comment pages and allows a image
    to be written as IOS app-tile (each article can have it's own app-tile)

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -54,6 +54,10 @@ class PictureUploader < CarrierWave::Uploader::Base
      process :resize_to_fill => [250, 250]
    end
 
+    version :microthumb do
+     process :resize_to_fill => [50, 50]
+   end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
    def extension_white_list
