62f3d99013acb0d85d92c910661a1cd45a11ce42
amonfog <amonfog@gmail.com>
2013-05-21 16:22:01 +0200

    Fix fav-button
    moves "afkeuren" outside favorites-container and puts id in
    favorite-container

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,23 +1,23 @@
 // this is de default javascript file for the application. functions that should be implemented on all article pages go here
 $(document).ready(function() {
 	$(".favoritebutton").click(function() {
-	  var article_id = $(this).attr('id');
+	  var article_id = $(".favorite-container").attr('id');
 	  $.ajax({
 	    type: "POST",
 	    url: '/favorites/add/' + article_id,
 	    success: function() {
-	      $(".favorite-container").empty().append("<div class='unfavoritebutton' id='<%= @article.id %>'></div>")
+	      $(".favorite-container").empty().append("<div class='unfavoritebutton'></div>")
 	    },
 	  })
 	})
 
 	$(".unfavoritebutton").click(function() {
-	  var article_id = $(this).attr('id');
+	  var article_id = $(".favorite-container").attr('id');
 	  $.ajax({
 	    type: "post",
 	    url: '/favorites/destroy/' + article_id,
 	    success: function() {
-	      $(".favorite-container").empty().append("<div class='favoritebutton' id='<%= @article.id %>'></div>")
+	      $(".favorite-container").empty().append("<div class='favoritebutton'></div>")
 	    },
 	  })
 	})
