365d3552937ed395ae0c30ba8dba7994dcd23c39
amonfog <amonfog@gmail.com>
2013-05-27 14:56:37 +0200

    Add sync tools for server

---
+++ config/deploy.rb
@@ -0,0 +1,22 @@
+require 'bundler/capistrano'
+load 'deploy/assets'
+ 	
+set :application, "inspiratieplanet"
+set :ip_address, "149.210.131.191"
+ 
+set :deploy_to, "/home/deploy/#{application}"
+set :user, 'deploy'
+set :use_sudo, false
+ 
+set :scm, :git
+set :repository, "git@github.com:amonfog/inspiratieplanet.git"
+set :branch, "master"
+set :repository_cache, "git_cache"
+set :deploy_via, :remote_cache
+set :ssh_options, { :forward_agent => true }
+ 
+default_run_options[:pty] = true
+ 
+role :web, ip_address
+role :app, ip_address
+role :db,  ip_address, :primary => true
