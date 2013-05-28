eb6fe5a9122520bf2c87e101def4e1f6573db6d0
Tjerk Jansma <tjerkjansma@msn.com>
2013-05-28 15:16:07 +0200

    placeholder in progress
    placeholder avatar doesn't work yet

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -20,8 +20,8 @@ class PictureUploader < CarrierWave::Uploader::Base
    include CarrierWave::MiniMagick
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
-  # include Sprockets::Helpers::RailsHelper
-  # include Sprockets::Helpers::IsolatedHelper
+  include Sprockets::Helpers::RailsHelper
+  include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -35,19 +35,19 @@ class PictureUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
+   def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #     asset_path("/images/users/" + [version_name, "/images/users/avatar_placeholder.png"].compact.join('_'))
   #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+   "/images/users" + [version_name, "/public/images/users/avatar_placeholder.png"].compact.join('_')
+   end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
+    process :resize_to_fill => [500, 500]
+  # #
+  # # def scale(width, height)
+  # #   # do something
+  #  end
 
   # Create different versions of your uploaded files:
    version :thumb do
